42a2bfa5cec49d8c8f82929de1139258e999f182
KodAndN <andrkokaz@gmail.com>
2022-12-03 09:52:10 +0300

    Post VC

--- Navigation3/Profile/PostVK.swift
+++ Navigation3/Profile/PostVK.swift
@@ -17,9 +17,10 @@ struct PostVK {
     static func makePost() -> [PostVK] {
         var model = [PostVK]()
         
-        model.append(PostVK(author: "В Мире Животных", description: "Кот", image: "img", like: 2, view: 12))
-        model.append(PostVK(author: "Андрей", description: "Андрей", image: "Photo1", like: 231, view: 12345))
-        
+        model.append(PostVK(author: "В Мире Животных", description: "Кот", image: "img", like: 210003, view: 100500))
+        model.append(PostVK(author: "Андрей", description: "Андрей", image: "Photo1", like: 100499, view: 100500))
+        model.append(PostVK(author: "Все инструменты", description: "MAKITA", image: "ВсеИ", like: 100488, view: 100500))
+        model.append(PostVK(author: "Cannondale BIKE", description: "2023 Cannondale Topstone Carbon Rival AXS review", image: "Cannondale-Topstone-Carbon-Rival-AXS-01-fb9ae59", like: 100500, view: 100500))
         return model
     }
 }

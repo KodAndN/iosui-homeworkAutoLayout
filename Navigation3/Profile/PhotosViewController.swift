ded2ac2277c6fc6cea793fd57c791458f3897558
KodAndN <andrkokaz@gmail.com>
2022-12-05 09:13:20 +0300

    Photos collection

---
+++ Navigation3/Profile/PhotosViewController.swift
@@ -0,0 +1,53 @@
+//
+//  PhotosViewController.swift
+//  Navigation3
+//
+//  Created by Андрей Кодочигов on 04.12.2022.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    private lazy var layout: UICollectionViewFlowLayout = {
+        $0.scrollDirection = .vertical
+        $0.minimumLineSpacing = 8
+        $0.minimumInteritemSpacing = 8
+        return $0
+    }(UICollectionViewFlowLayout())
+    
+    private lazy var collectionView: UICollectionView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .white
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier:
+            PhotosCollectionViewCell.identifier)
+        return $0
+    }(UICollectionView(frame: .zero, collectionViewLayout: self.layout))
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemGray6
+        self.title = "Фото Галерея"
+        self.navigationController?.navigationBar.isHidden = false
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+}
